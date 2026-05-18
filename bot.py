1ce8f2cf791c67464edbeb1fad77bf34af58397c
Jeremy Paredes <jeremyptpt02@gmail.com>
2026-05-18 14:31:02 -0500

    add multiple admins

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_ID = [7790388507, 8372332318]
+ADMIN_ID =7790388507, 8372332318
 
 
 # =========================
