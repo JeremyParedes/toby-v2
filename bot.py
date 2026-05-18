e77aed0d6891ae74f38fb718087034321b6c2d63
Jeremy Paredes <jeremyptpt02@gmail.com>
2026-05-18 15:11:08 -0500

    add multiple admins

--- bot.py
+++ bot.py
@@ -6,85 +6,72 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 ADMIN_ID = [7790388507, 8372332318]
 
 
-    # =========================
-    # COMANDO /start
-    # =========================
 async def inicio(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        print(update.effective_chat.id)
-        await update.message.reply_text("TOBY activo 😎")
+    print(update.effective_chat.id)
+    await update.message.reply_text("TOBY activo 😎")
 
 
-    # =========================
-    # COMANDO /cliente
-    # =========================
 async def cliente(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-        # IMPORTS SOLO CUANDO SE USAN (evita crash en deploy)
-        from app.database import SessionLocal
-        from app.models import Prestamo, Pago
+    from app.database import SessionLocal
+    from app.models import Prestamo, Pago
 
-        if update.effective_chat.id not in ADMIN_ID:
-            await update.message.reply_text("No autorizado")
+    if update.effective_chat.id not in ADMIN_ID:
+        await update.message.reply_text("No autorizado")
+        return
+
+    if not context.args:
+        await update.message.reply_text("Usa: /cliente nombre")
+        return
+
+    nombre = context.args[0]
+
+    db = SessionLocal()
+
+    try:
+        prestamo = db.query(Prestamo).filter(
+            Prestamo.cliente == nombre
+        ).first()
+
+        if not prestamo:
+            await update.message.reply_text("Cliente no encontrado")
             return
 
-        if not context.args:
-            await update.message.reply_text("Usa: /cliente nombre")
-            return
+        pagos = db.query(Pago).filter(
+            Pago.prestamo_id == prestamo.id
+        ).all()
 
-        nombre = context.args[0]
+        restante = prestamo.deuda_total - prestamo.pagado
 
-        db = SessionLocal()
+        mensaje = f"""
+🏦 ESTADO DE CUENTA
 
-        try:
-            prestamo = db.query(Prestamo).filter(
-                Prestamo.cliente == nombre
-            ).first()
+👤 Cliente: {prestamo.cliente}
+💰 Capital: S/{prestamo.monto}
+📊 Total: S/{prestamo.deuda_total}
+💵 Pagado: S/{prestamo.pagado}
+📉 Restante: S/{restante}
 
-            if not prestamo:
-                await update.message.reply_text("Cliente no encontrado")
-                return
+Estado: {prestamo.estado}
 
-            pagos = db.query(Pago).filter(
-                Pago.prestamo_id == prestamo.id
-            ).all()
+🏦 Historial de pagos:
+"""
 
-            restante = prestamo.deuda_total - prestamo.pagado
+        for pago in pagos:
+            mensaje += f"\n• {pago.fecha_pago} → S/{pago.monto_pago}"
 
-            mensaje = f"""
-    🏦 ESTADO DE CUENTA
+        await update.message.reply_text(mensaje)
 
-    👤 Cliente: {prestamo.cliente}
-    💰 Capital: S/{prestamo.monto}
-    📊 Total: S/{prestamo.deuda_total}
-    💵 Pagado: S/{prestamo.pagado}
-    📉 Restante: S/{restante}
-
-    Estado: {prestamo.estado}
-
-    🏦 Historial de pagos:
-    """
-
-            for pago in pagos:
-                mensaje += f"\n• {pago.fecha_pago} → S/{pago.monto_pago}"
-
-            await update.message.reply_text(mensaje)
-
-        finally:
-            db.close()
+    finally:
+        db.close()
 
 
-    # =========================
-    # APP TELEGRAM
-    # =========================
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", inicio))
 app.add_handler(CommandHandler("cliente", cliente))
 
 
-    # =========================
-    # MAIN
-    # =========================
 if __name__ == "__main__":
-        print("BOT TOBY ENCENDIDO 😎")
-        app.run_polling()
+    print("BOT TOBY ENCENDIDO 😎")
+    app.run_polling()
